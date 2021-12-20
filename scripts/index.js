e4b4353753ffcfaebe82ccded7f5615cf1ae005e
Yuliya Padzheva <yippee.smile@yandex.ru>
2021-12-20 12:57:08 +0300

    fixed js

--- scripts/index.js
+++ scripts/index.js
@@ -23,18 +23,9 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    nameInput = nameInput.value;
-    jobInput = jobInput.value;
-
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameInput;
-    profileJob.textContent = jobInput;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 }
-
 
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
