38e640f8220e62f2a0c3c5e4faba67fb7c7054fa
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-01-14 14:15:17 +0300

    add card template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,48 @@
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const list = document.querySelector('.gallery__items')
+
+initialCards.forEach(function(cardData) {
+    console.log('cardData=>', cardData)
+
+    const cardTemplate = document.querySelector('.card-template').content
+    const cardElement = cardTemplate.cloneNode(true)
+
+    const cardImage = cardElement.querySelector('.gallery__image')
+    const cardTitle = cardElement.querySelector('.gallery__title')
+
+    cardImage.src = cardData.link
+    cardTitle.textContent = cardData.name
+
+    list.prepend(cardElement)
+
+})
+
+
 //Открытие попапа
 
 const popupOpenButton = document.querySelector('.profile__edit-button');
